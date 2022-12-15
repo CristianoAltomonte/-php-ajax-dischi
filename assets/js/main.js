a20af7a13a636726d9417b919e6a444661be89c8
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-12-15 14:44:41 +0000

    creazione api

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        arrayDischi: [],
+
+    },
+    mounted() {
+
+        axios.get('./api/index.php')
+
+            .then((res) => {
+
+                this.arrayDischi = res.data
+
+            }
+            )
+    },
+    beforeUpdate() {
+
+    },
+    methods: {
+
+    }
+})
